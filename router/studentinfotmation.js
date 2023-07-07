4553caf147ddbff13abf183327cb26526b8b4d9e
lixinping-github <lxp775399418@163.com>
2023-07-07 12:40:14 +0800

    提交原始文件

---
+++ router/studentinfotmation.js
@@ -0,0 +1,9 @@
+const express =require("express");
+const router= express.Router();//生成实例
+
+
+
+
+
+
+module.exports=router;//设置暴露
---
+++ router/userlogin.js
@@ -0,0 +1,13 @@
+const express =require("express");
+const router= express.Router();
+
+
+
+
+
+
+module.exports=router;//设置暴露
+
+
+
+
